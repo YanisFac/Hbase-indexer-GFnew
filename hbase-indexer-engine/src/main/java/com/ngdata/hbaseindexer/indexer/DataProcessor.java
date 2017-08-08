8491c79a8a78b89418b50c9c8cfe30c43c7b9d84
Sioa <iamsioa@163.com>
2017-08-08 16:36:13 +0800

    modified

---
+++ hbase-indexer-engine/src/main/java/com/ngdata/hbaseindexer/indexer/DataProcessor.java
@@ -0,0 +1,62 @@
+package com.ngdata.hbaseindexer.indexer;
+
+import org.apache.solr.common.SolrInputDocument;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class DataProcessor {
+    List<RowData> dataList = null;
+    Map<String , SolrInputDocument> contentData = null;
+    Pattern timePattern  = Pattern.compile("(\\d{4}(-)\\d{2}(-)\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\s?)");
+
+    DataProcessor(Map<String, SolrInputDocument> data , List<RowData> rowDataList){
+        dataList = rowDataList;
+        contentData = data;
+    }
+
+    public Map <String , SolrInputDocument> dealWithInputData(){
+        Map<String, SolrInputDocument> modifiedDataMap  = null ;
+        contentData.forEach((k,v) -> {
+            String name = k;
+            SolrInputDocument document = new SolrInputDocument();
+            Collection<String> keyNames = v.getFieldNames();
+            keyNames.forEach(fieldName -> {
+                String itemValue = v.getFieldValue(fieldName).toString();
+                if(timePattern.matcher(itemValue).groupCount() > 0){
+                    String formatDateTime = Convert2Solr(itemValue,"yyyy-MM-dd HH:mm:ss");
+                    document.addField(fieldName, formatDateTime);
+                }else{
+                    document.addField(fieldName, v.getFieldValue(fieldName));
+                }
+            });
+            modifiedDataMap.put(name, document);
+        });
+        return modifiedDataMap;
+    }
+
+    public void dealWithRowData(List<RowData> rowDataList) {
+    /*
+    * This function is waiting for completed, which is used to deal with
+    * the original data that is got from hbase and used to add indexer in
+    * solr.
+    * */
+    }
+
+    public String Convert2Solr(String date, String format){
+        String value = "";
+        try {
+            SimpleDateFormat fmt = new SimpleDateFormat(format);
+            Date _date = fmt.parse(date);
+            val calendar = Calendar.getInstance;
+            calendar.setTime(_date)
+            calendar.add(10, -8)
+            val df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ROOT)
+            value = df.format(calendar.getTime)
+        } catch {
+            case var7: Exception =>
+        }
+        value
+    }
+}
